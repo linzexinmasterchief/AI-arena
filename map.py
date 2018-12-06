3900be8374238ec3fbc23fad586e458f5039919b
Michael <linzexinmasterchief@gmail.com>
2018-12-06 10:54:23 +0800

    a star algorithm completed and tested

--- map.py
+++ map.py
@@ -18,3 +18,36 @@ class SquareGrid:
         results = filter(self.in_bounds, results)
         results = filter(self.passable, results)
         return results
+
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
+
+
+def draw_map(map, path):
+    for x in range(map.height):
+        for y in range(map.width):
+            label = " "
+            pos = (x, y)
+            if pos in path:
+                label = "@"
+            elif map.passable(pos):
+                label = "."
+            else:
+                label = "#"
+            print(label, end=" ")
+        print()
+
+
+# testing
+from find_path import *
+m = GridWithWeights(30, 15)
+m.walls = [(2,2), (2,3), (2,4), (3,3), (3,4), (6,7), (7,7), (6,8), (7,8), (7,9)]
+
+path = a_star_search(m, (0, 0), (17, 9))
+draw_map(m, path)
