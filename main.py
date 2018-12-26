4cd982edd77ae4400c4a1a92c859d66c5d25f825
Michael <linzexinmasterchief@gmail.com>
2018-12-26 17:16:08 +0800

    add comments

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ from picToMap import *
 
 pygame.init()
 
+# set map size
 map_size = (30, 30)
 
+# create paint screen
 screen = pygame.display.set_mode((map_size[0] * 10, map_size[1] * 10))
+# set window title
 pygame.display.set_caption("AI_playground")
 
+# create thread clocks and continue flags
 game_clock = pygame.time.Clock()
 game_continue = True
 physics_clock = pygame.time.Clock()
@@ -23,6 +27,7 @@ physics_continue = True
 graphics_clock = pygame.time.Clock()
 graphics_continue = True
 
+# preset color set
 ORANGE = (255, 200, 0)
 BLUE = (50, 150, 255)
 RED = (255, 0, 0)
@@ -30,13 +35,15 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 GREY = (200, 200, 200)
 
+# initialize predator positions
 predator1 = bot(center_position=[1, 1])
 predator2 = bot(center_position=[29, 1])
 predator3 = bot(center_position=[20, 20])
+# organize predators into a predator list
 predatorList = [predator1, predator2, predator3]
 
+# initialize target position
 target = bot(center_position=[29, 29])
-
 
 
 # the size of each grid is 10x10 -> map cordinate = pixel cordinate / 10
@@ -47,9 +54,8 @@ m.walls = []
 for y in range(len(mapArray)):
     for x in range(len(mapArray[y])):
         if mapArray[x][y] < 255:
-            # map correction
+            # map direction correction
             m.walls.append((29 - y, x))
-path = a_star_search(m, (predator1.get_pos()[0], predator1.get_pos()[1]), (target.get_pos()[0], target.get_pos()[1]))
 
 def graphics_thread():
     global target, m, predatorList
@@ -85,15 +91,19 @@ def physics_thread():
         else:
             target.set_pos((mouse_pos_x, mouse_pos_y))
 
-    
+        # for each predator
         for predator in predatorList:
-            
+            # find path to target
             path = a_star_search(m, (predator.get_pos()[0], predator.get_pos()[1]), (target.get_pos()[0], target.get_pos()[1]))
             predator.pathToTarget = path
+            # if didn't reach target and allow to move
             if len(predator.pathToTarget) > 1 and predator.timer > predator.get_move_duration():
+                # move to the next node on path
                 predator.set_pos(predator.pathToTarget[1])
+                # reset timer
                 predator.timer = 0
             else:
+                # wait for timer to reach move duration limit
                 predator.timer += 1
             if predator.get_pos() == target.get_pos():
                 print("catched!")
@@ -115,10 +125,13 @@ while game_continue:
     for event in pygame.event.get():
         # window exit
         if event.type == pygame.QUIT:
+            # exit all threads
             graphics_continue = False
             physics_continue = False
             game_continue = False
-            pygame.quit()
-            sys.exit()
     
     game_clock.tick(60)
+
+# quit window
+pygame.quit()
+sys.exit()
